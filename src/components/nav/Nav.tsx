0ba21c7dbc704f823ff67cbd3083887552eb9182
jobearry <golimlim2209@gmail.com>
2025-11-08 22:01:10 +0800

    change styling for carousel component for better aspect ratio
    updated project model

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -8,8 +8,8 @@ export const Nav = () => {
   return (
     <nav className="m-2">
       <ul className="grid grid-cols-3 place-items-center gap-2">
-        {navLinks.map(navLink => (
-          <li className="border w-full rounded-md h-10 h-max-12">
+        {navLinks.map((navLink,index) => (
+          <li key={index} className="border w-full rounded-md h-10 h-max-12">
             <a href={navLink.link} className="w-full h-full grid place-items-center">{navLink.name}</a>
           </li>
         ))}
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "../ui/card";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "../ui/carousel";
-import { sdmiProjects } from "./poject.config";
+import { sdmiProjects } from "./poject.model";
 
 export const Projects = () => {
   return (
@@ -11,12 +11,12 @@ export const Projects = () => {
             <CarouselItem key={x.id} 
               className="grid place-items-center md:basis-1/2">
                 <div className="p-1">
-                  <Card className="bg-transparent my-2 text-gray-400 aspect-3/4 sm:aspect-4/5 md:aspect-square">
+                  <Card className="bg-transparent my-2 text-gray-400">
                     <CardHeader>
                       <CardTitle className="font-bold text-[#ffffff] text-xl py-2 text-start">{x.title}</CardTitle>
                     </CardHeader>
-                    <CardContent className="flex-1 sm:overflow-hidden">
-                        <ul className="list-none text-start">
+                    <CardContent className="flex-1 sm:overflow-auto hover:scrollbar-thin scrollbar-hide aspect-square sm:aspect-video">
+                        <ul className="list-none text-start object-fill">
                           <li className="list-item">
                             <p><span className="font-bold">Role: </span>{x.role}</p>
                           </li>
@@ -24,17 +24,19 @@ export const Projects = () => {
                             <p><span className="font-bold">Duration: </span>{x.duration}</p>
                           </li>
                           <li className="list-item">
-                            <p className="">
-                              <span className="font-bold">Contribution: </span> 
-                              {x.description}
-                            </p>
+                            <span className="font-bold">Contribution: </span> 
+                            <ul className="list-disc ml-5">
+                              {x.contirbution.map((contrib,index) => (
+                                <li key={index}>{contrib}</li>
+                              ))}
+                            </ul>
                           </li>
                         </ul>
                     </CardContent>
                     <CardFooter>
                       <p className="p-2 text-end w-full">
-                        {x.tags.map(tag => (
-                          <span className="inline-block bg-orange-900 rounded-full px-3 py-1 text-sm font-semibold text-gray-300 mr-2 mb-2">
+                        {x.tags.map((tag,index) => (
+                          <span key={index} className="inline-block bg-orange-900 rounded-full px-3 py-1 text-xs font-semibold text-gray-300 mr-2 mb-2">
                             {tag}
                           </span>
                         ))}
