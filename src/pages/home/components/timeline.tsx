dd3a6504766378a8fd39d696d73c26b6d075daf8
Jonathan Golimlim <golimlim2209@gmail.com>
2025-12-31 15:14:00 +0800

    enhance Timeline component styles and add hover effect to event dots

--- src/pages/home/components/timeline.tsx
+++ src/pages/home/components/timeline.tsx
@@ -12,9 +12,10 @@ export const Timeline: React.FC<TimelineProps> = ({events}) => {
   return (              
     <div className="relative border-l-2 border-orange-700 ml-5">
       {events.map((event, index) => (
-        <div key={index} className="mb-8 ml-[1.9rem] relative">
+        <div key={index} className="mb-8 ml-[1.93rem] relative group">
           {/* Dot */}
-          <span className="absolute -left-10 w-4 h-4 bg-orange-700 rounded-full"></span>
+          <span className="absolute -left-10 w-4 h-4 rounded-full border-2 
+            transition-all duration-300 border-orange-700 bg-[#141414] group-hover:bg-orange-700"></span>
           
           {/* Content */}
           <div className="rounded mr-5 text-start">
